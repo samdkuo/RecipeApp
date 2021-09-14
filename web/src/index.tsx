8960d97fe7b9426d725d5b1a835050e5f9459c50
Sam Kuo <samanthakuo97@gmail.com>
2021-09-14 09:04:05 -0700

    feat: app loads properly

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,21 @@
-import React, { lazy } from "react";
+import React, { lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 const App = lazy(() => import("./App"));
 
+// const colorScheme =
+//   (localStorage.getItem("colorScheme") as ThemeMode) ?? "dark";
+// const themeName = localStorage.getItem("themeName") ?? "smartrent";
+// const theme = (themes as any)[themeName];
+
 render(
   // <ThemeProvider initialMode={colorScheme} theme={theme}>
-  //   <Suspense fallback={<FullScreenLoader />}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  //   </Suspense>
+  <Suspense fallback={<div>loading...</div>}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Suspense>,
   // </ThemeProvider>,
   document.getElementById("root")
 );
